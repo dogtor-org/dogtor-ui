236502c3635f676893d9c3e450688815bdc0dff8
Vinicius <vinireis010305@gmail.com>
2023-02-01 23:46:12 -0300

    Criando o estado do usuario

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,8 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+import {  useState } from 'react'
 
 import { Header } from './components/Header'
 
 export function Register(){
+  const [user,setUser] = useState({})
+
+  function updateUser(data: object){
+    setUser((prevUser) => ({...prevUser, ...data}))
+  }
 
   return(
     <div className="register-container">
