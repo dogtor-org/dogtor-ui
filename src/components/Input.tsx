83db00c21e00e3ec8cafa41311698648af86e57c
Vinicius <vinireis010305@gmail.com>
2023-02-01 23:35:20 -0300

    Alterando o component Input para receber valores e mudalos

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,12 +5,18 @@ interface IInput {
   icon?: React.ReactNode
   placeholder: string,
   onClick?(): void,
+  value?: string,
+  onChange?(event: React.FormEvent<HTMLInputElement>): void,
+  id?: string
 }
 
-export const Input = React.forwardRef<HTMLInputElement, IInput>(({ type, icon, placeholder, onClick }, ref: React.Ref<HTMLInputElement>) => {
+export const Input = React.forwardRef<HTMLInputElement, IInput>(({ type, icon, placeholder, onClick, value, onChange, id }, ref: React.Ref<HTMLInputElement>) => {
   return (
     <div className='input-container'>
       <input
+        id={id}
+        value={value}
+        onChange={onChange}
         type={type}
         placeholder={placeholder}
         ref={ref} />
