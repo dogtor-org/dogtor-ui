1c909b988ff1e2f07f91d4da0a945064b1423996
Vinicius <vinireis010305@gmail.com>
2023-02-01 23:41:30 -0300

    Criando o index da page Register

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
+import { Header } from './components/Header'
+
+export function Register(){
+
+  return(
+    <div className="register-container">
+      <Header />
+      <div className='register-content'>
+      </div>
+    </div>
+  )
+}
