07c5e36476c460fa0ef8a0887d711f928924b4c4
Vinicius <vinireis010305@gmail.com>
2023-02-02 22:32:07 -0300

    Adicionado rota para a pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
 import { Register } from "./pages/Register"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register/*" element={<Register/>}/>
+        <Route path='/home' element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/register/*" element={<Register />} />
+      </Routes>
       </Routes>
     </BrowserRouter>
   )
