5fe00c0a0bd5515ac989fa9bc63d06dc1d73d351
Vinicius <vinireis010305@gmail.com>
2023-02-02 22:30:20 -0300

    Adicionado a seção de DogCard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import dogLeft from '../../assets/imgs/dog-welcome-home.png'
 import dogOwner from '../../assets/imgs/dog&owner-welcome-home.png'
 import { Treatment } from '../../components/Treatment'
+import { DogCard } from '../../components/DogCard'
+
 export function Home() {
   return (
     <>
@@ -24,6 +26,16 @@ export function Home() {
             <Treatment />
             <Treatment />
             <Treatment />
+          </div>
+        </section>
+
+        <section className='home__section-my-dogs'>
+          <span className='home__section-my-dogs__text-container'>
+            <h2>Seus  Cachorros</h2>
+            <button className='home__section-my-dogs__text-container__add-dog'>Adiconar</button>
+          </span>
+          <div className="card-dogs">
+            <DogCard />
           </div>
         </section>
       </main>
